061905e16f0ab5a67f714dad40c3788a7befcb74
Daoyuan <1833063210@qq.com>
2018-12-18 14:14:55 +0800

    优化集群配置

--- server/README-CONF.md
+++ server/README-CONF.md
@@ -29,27 +29,34 @@ eureka:
 
 ### 单机配置多个host [windows篇]
 - 路径：C:\Windows\System32\drivers\etc
-- 修改hosts文件：127.0.0.1 slave1 slave2
+- 修改hosts文件：127.0.0.1 slave1 slave2 slave3
 
 ### 项目打包与启动
-- mvn package 生成 server.jar
+- 生成 server.jar
+  - mvn package
 - 启动 server.jar
-- java -jar server.jar
-- 请输入运行环境 (slave1 or slave2):
-- slave1 或者 slave2
+  - java -jar server.jar
+- 请选择运行环境[s1, s2, s3]:
+  - s1 或者 s2 或者 s3
 
 ### 配置文件内容
 ```yaml
 spring:
   application:
     name: mall-server
+
 eureka:
   client:
     service-url:
-      defaultZone: http://slave1:8761/eureka,http://slave2:8762/eureka
+      defaultZone: http://slave1:8761/eureka,http://slave2:8762/eureka,http://slave3:8763/eureka
+
+info:
+  app:
+    name: mall-server
+    description: Eureka 注册中心
+    version: 1.0.0
 
 ---
-# 主机名 slave1
 spring:
   profiles:
     slave1
@@ -60,7 +67,6 @@ eureka:
     hostname: slave1
 
 ---
-# 主机名 slave2
 spring:
   profiles:
     slave2
@@ -69,4 +75,14 @@ server:
 eureka:
   instance:
     hostname: slave2
+
+---
+spring:
+  profiles:
+    slave3
+server:
+  port: 8763
+eureka:
+  instance:
+    hostname: slave3
 ```
--- server/src/main/java/com/mall/server/ServerApplication.java
+++ server/src/main/java/com/mall/server/ServerApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  * 商城系统-注册中心
@@ -12,15 +14,37 @@ import java.util.Scanner;
  * @author Daoyuan
  * @version 1.0.0
  */
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication
 public class ServerApplication {
 
+    private static final Map<String, String> ENV_MAP = new TreeMap<>();
+
+    static {
+        ENV_MAP.put("s1", "slave1");
+        ENV_MAP.put("s2", "slave2");
+        ENV_MAP.put("s3", "slave3");
+    }
+
+    /**
+     * 最大错误次数
+     */
+    private static final int ERROR_COUNT = 5;
+
     public static void main(String[] args) {
-        System.out.print("请输入运行环境 (slave1 or slave2):");
-        Scanner scan = new Scanner(System.in);
-        String profiles = scan.nextLine();
-        new SpringApplicationBuilder(ServerApplication.class).profiles(profiles).run(args);
-        /*SpringApplication.run(ServerApplication.class, args);*/
+        int errorCount = 0;
+        while (errorCount < ERROR_COUNT) {
+            System.out.print("请选择运行环境" + ENV_MAP.keySet() + ": ");
+            String profiles = new Scanner(System.in).nextLine();
+            if (ENV_MAP.containsKey(profiles)) {
+                profiles = ENV_MAP.get(profiles);
+            } else {
+                System.out.println("请输入正确的运行环境呦~~~");
+                errorCount++;
+                continue;
+            }
+            new SpringApplicationBuilder(ServerApplication.class).profiles(profiles).run(args);
+        }
+        System.exit(0);
     }
 }
