9697e9637aad4281b43f0b0dec97031110d3e74a
amos.wang <amos.wang@xiaoi.com>
2020-03-07 21:18:10 +0800

    feat: 经测试 gateway lb://xxx原生支持负载均衡

--- mall-user/README.md
+++ mall-user/README.md
@@ -1,3 +1,6 @@
-# gateway
+# user
 
 ## 用户系统【积分、收获地址等】
+
+## IDEA单机启动多个User 
+- VM Option `-Dserver.port=8011`
--- mall-user/src/main/java/com/mall/user/web/controller/IntegralController.java
+++ mall-user/src/main/java/com/mall/user/web/controller/IntegralController.java
@@ -1,5 +1,6 @@
 package com.mall.user.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("integral")
 public class IntegralController {
 
+    @Value("${server.port}")
+    private Integer port;
+
     @GetMapping("test")
     public String test() {
         return "用户积分处理完成!";
+    }
+
+    @GetMapping("port")
+    public String port() {
+        return "当前服务端口: " + port;
     }
 
 }
