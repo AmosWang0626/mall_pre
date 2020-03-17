501f2c7303e7c79f176c9cedddeb760188b03752
amos.wang <amos.wang@xiaoi.com>
2020-03-17 18:32:33 +0800

    feat: gateway add mq provider

---
+++ mall-gateway/src/main/java/com/mall/gateway/core/mq/service/impl/MessageProviderImpl.java
@@ -0,0 +1,34 @@
+package com.mall.gateway.core.mq.service.impl;
+
+import com.mall.gateway.core.mq.service.MessageProvider;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+
+import javax.annotation.Resource;
+
+
+/**
+ * 模块名称: MQ生产者
+ * 模块描述: 定义消息的推送管道 source
+ *
+ * @author amos.wang
+ * @date 2020/3/17 18:09
+ */
+@EnableBinding(Source.class)
+public class MessageProviderImpl implements MessageProvider {
+
+    @Resource
+    private MessageChannel output;
+
+    @Override
+    public String send(String message) {
+        boolean send = output.send(MessageBuilder.withPayload(message).build());
+        String status = send ? "成功" : "失败";
+        System.out.printf("发送消息 [%s] 状态: %s\n", message, status);
+
+        return status;
+    }
+
+}
---
+++ mall-gateway/src/main/java/com/mall/gateway/web/controller/MqController.java
@@ -0,0 +1,43 @@
+package com.mall.gateway.web.controller;
+
+import com.mall.common.base.GenericResponse;
+import com.mall.gateway.core.mq.service.MessageProvider;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
+
+/**
+ * PROJECT: gateway
+ * DESCRIPTION: note
+ *
+ * @author Daoyuan
+ * @date 2018/12/11
+ */
+@RestController
+@RequestMapping("mq")
+@Api(tags = "C01-MQ测试")
+public class MqController {
+
+    @Resource
+    private MessageProvider messageProvider;
+
+
+    /**
+     * 通过 MQ 发送消息
+     *
+     * @param message 表单
+     * @return Mono
+     */
+    @PostMapping(value = "send/{message}")
+    @ApiOperation(value = "登录接口")
+    public Mono<GenericResponse<String>> send(@PathVariable("message") String message) {
+        return Mono.just(new GenericResponse<>(messageProvider.send(message)));
+    }
+
+}
